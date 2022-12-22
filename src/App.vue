ede01e50839126239c35c36cd2816cd1be05a56c
Ian Paul <ian@ecocart.io>
2022-12-21 19:21:22 -0800

    Adding header

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-content>
+      <v-container align-center>
+        <TheHeaderVue :goDark="goDark" @changeTheme="updateTheme($event)" />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view />
-    </v-main>
+        <transition
+          name="router-animation"
+          mode="out-in"
+          enter-active-class="animated fadeIn fast"
+          leave-active-class="animated fadeOut faster"
+        >
+          <router-view></router-view>
+        </transition>
+      </v-container>
+    </v-content>
     <TheFooterVue />
   </v-app>
 </template>
 
 <script>
 import TheFooterVue from './components/TheFooter.vue'
+import TheHeaderVue from './components/TheHeader.vue'
 
 export default {
   name: 'App',
@@ -50,7 +29,8 @@ export default {
     //
   }),
   components: {
-    TheFooterVue
+    TheFooterVue,
+    TheHeaderVue
   }
 }
 </script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      app
+      width="150"
+      height="340"
+    >
+      <v-list class="pt-4">
+        <v-list-tile active-class="blue--text" to="/">
+          <v-list-tile-content>
+            <v-list-tile-title>HOME</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile active-class="blue--text" to="/recipes">
+          <v-list-tile-content>
+            <v-list-tile-title>RECIPES</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile active-class="blue--text" to="/tags">
+          <v-list-tile-content>
+            <v-list-tile-title>TAGS</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile active-class="blue--text" to="/about">
+          <v-list-tile-content>
+            <v-list-tile-title>ABOUT</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar flat dense color="transparent" scroll-off-screen>
+      <v-toolbar-side-icon
+        class="hidden-md-and-up"
+        @click.stop="drawer = !drawer"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title class="headline">
+        <span class="font-weight-light">Paul Family </span>
+        <span class="blue--text">Cookbook</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="changeTheme" depressed small icon class="hidden-md-and-up">
+        <v-icon v-if="goDark == true">fas fa-sun</v-icon>
+        <v-icon v-else>fas fa-moon</v-icon>
+      </v-btn>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat to="/" active-class="blue--text headline">Home</v-btn>
+        <v-btn flat to="/recipes" active-class="blue--text headline"
+          >Recipes</v-btn
+        >
+        <v-btn flat to="/tags" active-class="blue--text headline">Tags</v-btn>
+        <v-btn flat to="/about" active-class="blue--text headline">About</v-btn>
+        <v-btn @click="changeTheme" depressed small icon>
+          <v-icon v-if="goDark == true">fas fa-sun</v-icon>
+          <v-icon v-else>fas fa-moon</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    goDark: {
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+      drawer: null
+    }
+  },
+  methods: {
+    changeTheme() {
+      this.$emit('changeTheme', this.goDark)
+    }
+  }
+}
+</script>
+
+<style></style>
